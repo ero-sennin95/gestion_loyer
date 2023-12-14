2f89962a0541766b7bd8300f8e45b78d9b1e92fc
david <davidxyz95bb@hotmail.com>
2023-12-14 21:48:34 +0100

    premier envoi

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Version 1</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet labore possimus veritatis alias, nulla voluptatem debitis laborum reiciendis provident sit minus, culpa consectetur impedit soluta repellendus ad! Nisi, perferendis enim.</p>
+</body>
+</html>
